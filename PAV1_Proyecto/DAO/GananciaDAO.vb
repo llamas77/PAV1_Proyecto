c52abf9a3c3b2cb07e8bb71486cef13009e5caa1
Juani <wfalconwf@gmail.com>
2017-05-14 13:34:42 -0300

    Esqueleto de Ganancia. GananciaVO Completo.

---
+++ PAV1_Proyecto/DAO/GananciaDAO.vb
@@ -0,0 +1,37 @@
+﻿Imports PAV1_Proyecto
+
+Public Class GananciaDAO
+    Implements ObjetoDAO, ObjectFactory
+
+    Public Sub insert(value As ObjetoVO) Implements ObjetoDAO.insert
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Sub update(value As ObjetoVO) Implements ObjetoDAO.update
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Sub delete(value As ObjetoVO) Implements ObjetoDAO.delete
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Function all() As DataTable Implements ObjetoDAO.all
+        Throw New NotImplementedException()
+    End Function
+
+    Public Function exists(value As ObjetoVO) As Boolean Implements ObjetoDAO.exists
+        Throw New NotImplementedException()
+    End Function
+
+    Public Function get_IU_control() As ControlGenerico Implements ObjetoDAO.get_IU_control
+        Throw New NotImplementedException()
+    End Function
+
+    Public Function get_IU_grilla() As GrillaGenerica Implements ObjetoDAO.get_IU_grilla
+        Throw New NotImplementedException()
+    End Function
+
+    Public Function new_instance(valores As Dictionary(Of String, Object)) As ObjetoVO Implements ObjectFactory.new_instance
+        Throw New NotImplementedException()
+    End Function
+End Class
---
+++ PAV1_Proyecto/VO/GananciaVO.vb
@@ -0,0 +1,27 @@
+﻿Imports PAV1_Proyecto
+
+Public Class GananciaVO
+    Implements ObjetoVO
+
+    Public Property _grupo As GrupoVO
+    Public Property _tipo_cliente As TipoClienteVO
+    Public Property _ganancia As Double
+
+    Public Sub New(grupo As GrupoVO, tipo_cliente As TipoClienteVO, ganancia As Double)
+        _grupo = grupo
+        _tipo_cliente = tipo_cliente
+        _ganancia = ganancia
+    End Sub
+
+    Public Function toDictionary() As Dictionary(Of String, Object) Implements ObjetoVO.toDictionary
+        Dim diccionario As New Dictionary(Of String, Object)
+        diccionario.Add("grupo", _grupo)
+        diccionario.Add("tipo_cliente", _tipo_cliente)
+        diccionario.Add("ganancia", _ganancia)
+        Return diccionario
+    End Function
+
+    Private Function ObjetoVO_toString() As String Implements ObjetoVO.toString
+        Return _grupo._nombre & " por " & _tipo_cliente._nombre & " (" & _ganancia * 100 & "%)"
+    End Function
+End Class
