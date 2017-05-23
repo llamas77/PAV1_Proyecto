6f6d2bca2aafac7a92a6f4ae4c291b399b2eec98
Juani <wfalconwf@gmail.com>
2017-05-23 15:29:37 -0300

    Agregado control LabeledTextBox. Tiene un mejor control sobre el tamaño de la entrada.

---
+++ PAV1_Proyecto/Forms/Componentes/UserControls/LabeledTextBox.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class LabeledTextBox
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lbl_texto = New System.Windows.Forms.Label()
+        Me.txt_caja = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'lbl_texto
+        '
+        Me.lbl_texto.Dock = System.Windows.Forms.DockStyle.Left
+        Me.lbl_texto.Location = New System.Drawing.Point(0, 0)
+        Me.lbl_texto.Name = "lbl_texto"
+        Me.lbl_texto.Size = New System.Drawing.Size(120, 28)
+        Me.lbl_texto.TabIndex = 1
+        Me.lbl_texto.Text = "DefaultText"
+        Me.lbl_texto.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'txt_caja
+        '
+        Me.txt_caja.Location = New System.Drawing.Point(125, 3)
+        Me.txt_caja.MaximumSize = New System.Drawing.Size(165, 22)
+        Me.txt_caja.MinimumSize = New System.Drawing.Size(4, 22)
+        Me.txt_caja.Name = "txt_caja"
+        Me.txt_caja.Size = New System.Drawing.Size(165, 22)
+        Me.txt_caja.TabIndex = 0
+        '
+        'LabeledTextBox
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.txt_caja)
+        Me.Controls.Add(Me.lbl_texto)
+        Me.Name = "LabeledTextBox"
+        Me.Size = New System.Drawing.Size(293, 28)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lbl_texto As Label
+    Friend WithEvents txt_caja As TextBox
+End Class
---
+++ PAV1_Proyecto/Forms/Componentes/UserControls/LabeledTextBox.vb
@@ -0,0 +1,82 @@
+﻿Imports PAV1_Proyecto
+
+Public Class LabeledTextBox
+    Inherits UserControl
+    Implements ObjetoCampo, Validable
+
+    '
+    '  -- Interfaz ObjetoCampo
+    '
+    Public Property _id As String Implements ObjetoCampo._id
+
+    Public Property _label As String Implements ObjetoCampo._label
+        Get
+            Return lbl_texto.Text
+        End Get
+        Set(ByVal value As String)
+            lbl_texto.Text = value
+            ' Asegura que el texto siempre sea visible.
+            If lbl_texto.PreferredSize.Width > lbl_texto.Width Then
+                Dim add As Integer = lbl_texto.PreferredSize.Width - lbl_texto.Width
+                lbl_texto.Width += add
+                txt_caja.Location = New Point(txt_caja.Location.X + add, txt_caja.Location.Y)
+            End If
+        End Set
+    End Property
+
+    Public Property _value As Object Implements ObjetoCampo._value
+        Get
+            Return txt_caja.Text.Trim()
+        End Get
+        Set(value As Object)
+            If TypeOf value Is String Then
+                txt_caja.Text = value
+            Else
+                Throw New System.Exception("Un TextBox solo acepta valores del tipo String")
+            End If
+        End Set
+    End Property
+
+    '
+    '  -- Interfaz Validable
+    '
+    Public Property _required As Boolean Implements Validable._required
+        Get
+            Throw New NotImplementedException()
+        End Get
+        Set(value As Boolean)
+            Throw New NotImplementedException()
+        End Set
+    End Property
+
+    Public Property _min_lenght As Integer Implements Validable._min_lenght
+        Get
+            Throw New NotImplementedException()
+        End Get
+        Set(value As Integer)
+            Throw New NotImplementedException()
+        End Set
+    End Property
+
+    Public Property _max_lenght As Integer Implements Validable._max_lenght
+        Get
+            Throw New NotImplementedException()
+        End Get
+        Set(value As Integer)
+            Throw New NotImplementedException()
+        End Set
+    End Property
+
+    Public Property _numeric As Boolean Implements Validable._numeric
+        Get
+            Throw New NotImplementedException()
+        End Get
+        Set(value As Boolean)
+            Throw New NotImplementedException()
+        End Set
+    End Property
+
+    Public Function is_valid() As Boolean Implements Validable.is_valid
+        Throw New NotImplementedException()
+    End Function
+End Class
