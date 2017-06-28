e85735a636f888e7033e8d853eb4fffcb50893ba
Franco Llamas <llamas077@hotmail.com>
2017-06-28 15:27:38 -0300

    Est Ganancias x tipo cliente

--- PAV1_Proyecto/Reportes/Estadisticas/FormsEst/repEstGananciasXTipoC.Designer.vb
+++ PAV1_Proyecto/Reportes/Estadisticas/FormsEst/repEstGananciasXTipoC.Designer.vb
@@ -22,21 +22,24 @@ Partial Class repEstGananciasXTipoC
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.panel_control = New System.Windows.Forms.FlowLayoutPanel()
         Me.btn_buscar = New System.Windows.Forms.Button()
         Me.ReportViewer1 = New Microsoft.Reporting.WinForms.ReportViewer()
+        Me.BindingSource1 = New System.Windows.Forms.BindingSource(Me.components)
+        CType(Me.BindingSource1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'panel_control
         '
         Me.panel_control.Location = New System.Drawing.Point(12, 12)
         Me.panel_control.Name = "panel_control"
-        Me.panel_control.Size = New System.Drawing.Size(497, 89)
+        Me.panel_control.Size = New System.Drawing.Size(497, 44)
         Me.panel_control.TabIndex = 25
         '
         'btn_buscar
         '
-        Me.btn_buscar.Location = New System.Drawing.Point(515, 78)
+        Me.btn_buscar.Location = New System.Drawing.Point(515, 33)
         Me.btn_buscar.Name = "btn_buscar"
         Me.btn_buscar.Size = New System.Drawing.Size(75, 23)
         Me.btn_buscar.TabIndex = 24
@@ -48,22 +51,23 @@ Partial Class repEstGananciasXTipoC
         Me.ReportViewer1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
             Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.ReportViewer1.Location = New System.Drawing.Point(12, 118)
+        Me.ReportViewer1.Location = New System.Drawing.Point(12, 62)
         Me.ReportViewer1.Name = "ReportViewer1"
         Me.ReportViewer1.ServerReport.BearerToken = Nothing
-        Me.ReportViewer1.Size = New System.Drawing.Size(685, 307)
+        Me.ReportViewer1.Size = New System.Drawing.Size(685, 290)
         Me.ReportViewer1.TabIndex = 23
         '
         'repEstGananciasXTipoC
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(709, 437)
+        Me.ClientSize = New System.Drawing.Size(709, 364)
         Me.Controls.Add(Me.panel_control)
         Me.Controls.Add(Me.btn_buscar)
         Me.Controls.Add(Me.ReportViewer1)
         Me.Name = "repEstGananciasXTipoC"
         Me.Text = "repEstGananciasXTipoC"
+        CType(Me.BindingSource1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -71,4 +75,5 @@ Partial Class repEstGananciasXTipoC
     Friend WithEvents panel_control As FlowLayoutPanel
     Friend WithEvents btn_buscar As Button
     Friend WithEvents ReportViewer1 As Microsoft.Reporting.WinForms.ReportViewer
+    Friend WithEvents BindingSource1 As BindingSource
 End Class
--- PAV1_Proyecto/Reportes/Estadisticas/FormsEst/repEstGananciasXTipoC.vb
+++ PAV1_Proyecto/Reportes/Estadisticas/FormsEst/repEstGananciasXTipoC.vb
@@ -1,3 +1,68 @@
 ﻿Public Class repEstGananciasXTipoC
+    Private Sub RepGrupos_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim ctrl_list As New List(Of Campo)
+        ctrl_list.Add(New Campo With {._id = "gan_min", ._name = "Ganancia Min.", ._numeric = True})
+        ctrl_list.Add(New Campo With {._id = "gan_max", ._name = "Ganancia Max.", ._numeric = True})
+        For Each campo In ctrl_list
+            panel_control.Controls.Add(campo.get_UserControl)
+        Next
+        buscar()
+    End Sub
 
+    Private Sub btn_buscar_Click(sender As Object, e As EventArgs) Handles btn_buscar.Click
+        buscar()
+    End Sub
+
+    Private Sub buscar()
+        Dim db = DataBase.getInstance()
+        Dim filtros As New Dictionary(Of String, Object)
+        For Each campo As ObjetoCampo In panel_control.Controls
+            filtros(campo._id) = campo._value
+        Next
+
+        Dim sql = ""
+        sql += "select tc.nombre , AVG(ganancia) as ganancia "
+        sql += "from tipos_cliente tc join ganancias g on tc.idTipo = g.idTipo "
+        sql += "group by tc.nombre "
+
+        Dim hay_having = False
+        If filtros("gan_min") <> 0 Then
+            sql &= IIf(hay_having, " AND ", " HAVING ")
+            sql &= " AVG(ganancia) >= " & filtros("gan_min")
+            hay_having = True
+        End If
+        If filtros("gan_max") <> 0 Then
+            sql &= IIf(hay_having, " AND ", " HAVING ")
+            sql &= " AVG(ganancia) <= " & filtros("gan_max")
+            hay_having = True
+        End If
+
+        Dim response As DataTable = db.consulta_sql(sql)
+
+
+        ReinitializeViewer("PAV1_Proyecto.repEstGananciasXTipoC.rdlc")
+        BindingSource1.DataSource = response
+        Me.ReportViewer1.RefreshReport()
+
+    End Sub
+
+    Private Sub repCompras_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        Select Case e.KeyCode
+            Case Keys.Escape
+                Me.Close()
+        End Select
+    End Sub
+
+    Private Sub ReinitializeViewer(ByVal tsReport As String)
+
+        Dim ReportDataSourceX = New Microsoft.Reporting.WinForms.ReportDataSource()
+
+        Me.ReportViewer1.Reset()
+        Me.ReportViewer1.LocalReport.ReportEmbeddedResource = tsReport
+        ReportDataSourceX.Name = "DataSet1"
+        ReportDataSourceX.Value = Me.BindingSource1
+
+        Me.ReportViewer1.LocalReport.DataSources.Add(ReportDataSourceX)
+        Me.ReportViewer1.Visible = True
+    End Sub
 End Class
